ef37b6d125ceb921cc298456ee4bf54440fb972b
DréGuerra <andreale.guerra58@gmail.com>
2022-11-30 22:16:14 +0000

    Remoção do firebase e do método de autenticação

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,17 @@ import { User } from "./pages/User";
 import { Games } from "./pages/Games";
 import { Room } from "./pages/Room";
 
-import { AuthContextProvider } from "./contexts/AuthContext";
-
 function App() {
   return (
     <BrowserRouter>
-      <AuthContextProvider>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/sobre" element={<About />}></Route>
-          <Route path="/login" element={<LoginRoom />}></Route>
-          <Route path="/user" element={<User />}></Route>
-          <Route path="/games" element={<Games />}></Route>
-          <Route path="/games/stray" element={<Room />}></Route>
-        </Routes>
-      </AuthContextProvider>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/sobre" element={<About />}></Route>
+        <Route path="/login" element={<LoginRoom />}></Route>
+        <Route path="/user" element={<User />}></Route>
+        <Route path="/games" element={<Games />}></Route>
+        <Route path="/games/stray" element={<Room />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
